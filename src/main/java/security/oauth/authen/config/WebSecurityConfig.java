bb3a69d4b456724412bed752b0c9853b7bec3cb1
oreopon <pon_engine@hotmail.com>
2018-08-24 12:58:53 +0700

    add option

--- src/main/java/security/oauth/authen/config/WebSecurityConfig.java
+++ src/main/java/security/oauth/authen/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -45,6 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	  	
         // @formatter:off
         http.authorizeRequests()
+        	.antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
             .anyRequest().authenticated()
             .and().formLogin()
             .and().csrf().disable();
